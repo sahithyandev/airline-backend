af395d275277f96f35b8c6ac900262d31c2643cc
Sahithyan <sahithyan2701@gmail.com>
2023-10-13 13:19:03 +0530

    close mysql connection on kill

--- src/index.js
+++ src/index.js
@@ -59,3 +59,9 @@ const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
 	console.log(`Server started on http://localhost:${PORT}`);
 });
+
+process.on("SIGINT", () => {
+	console.log("Closing connection to mysql");
+	mysql.end(console.error);
+	process.exit(0);
+});
